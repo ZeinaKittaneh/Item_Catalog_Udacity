ed4fb80e73942639236fd3f79bcccb58c42d573e
ZeinaKittaneh <36554989+ZeinaKittaneh@users.noreply.github.com>
2019-02-22 15:45:19 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Item-Catalog Udacity Full-Stack-NanoDegree Project
+This app is used to display categories of tools with some examples of each category, authenticated user can add, edit
+or delete items if he/she created.
+it also returns data in JSON format
+
+## Requirements
+In order to use this app you need to install the following dependencies:
+
+Flask
+SQLAlchemy
+and Flask-GoogleLogin
+
+use these commands:
+
+`pip install Flask`
+`pip install SQLAlchemy`
+`pip install Flask-GoogleLogin`
+
+## Running the app
+
+1. Setup the database:
+`python database_setupy.py`
+
+2. Fill in data:
+`python lotsOfCategories.py`
+
+3. Run main python project
+`python catalog.py`
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](https://github.com/arrickx/Item-Catalog-Application/blob/master/LICENSE) file for details.
+
---
+++ database_setup.py
@@ -0,0 +1,59 @@
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String, TIMESTAMP
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'user'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+    email = Column(String(250), nullable=False)
+    picture = Column(String(250))
+
+
+class Category(Base):
+    __tablename__ = 'category'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250), nullable=False)
+
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'id': self.id,
+        }
+
+
+class CategoryItem(Base):
+    __tablename__ = 'category_item'
+
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
+    description = Column(String(250))
+    created_at = Column(TIMESTAMP, nullable=False)
+    category_id = Column(Integer, ForeignKey('category.id'))
+    category = relationship(Category)
+    user_id = Column(Integer, ForeignKey('user.id'), nullable=False)
+    user = relationship(User)
+    
+    @property
+    def serialize(self):
+        """Return object data in easily serializeable format"""
+        return {
+            'name': self.name,
+            'description': self.description,
+            'id': self.id
+        }
+
+
+engine = create_engine('sqlite:///categories.db')
+
+
+Base.metadata.create_all(engine)
